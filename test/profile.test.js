1fb59342a25e897dda5cc750bd39f1d5b0f6bbdc
David Jimenez <djimenez@bluemessaging.com>
2016-08-31 17:16:52 -0700

    Update strategy to use the Portable Contacts format

--- test/profile.test.js
+++ test/profile.test.js
@@ -19,11 +19,12 @@ describe('profile.parse', function() {
     });
     
     it('should parse profile', function() {
-      expect(profile.Id).to.equal('0dbf6616-20bd-4cbd-860d-47c5b7953e76@c512ffd1-581d-4dc0-a672-faee32f6387c');
-      expect(profile.EmailAddress).to.equal('AllieB@oauthplay.onmicrosoft.com');
-      expect(profile.DisplayName).to.equal('Allie Bellew');
-      expect(profile.Alias).to.equal('AllieB');
-      expect(profile.MailboxGuid).to.equal('8d899a1e-bde4-4946-8817-005e6f11d36d');
+      expect(profile.id).to.equal('0dbf6616-20bd-4cbd-860d-47c5b7953e76@c512ffd1-581d-4dc0-a672-faee32f6387c');
+      expect(profile.emails[0].type).to.equal('home');
+      expect(profile.emails[0].value).to.equal('AllieB@oauthplay.onmicrosoft.com');
+      expect(profile.displayName).to.equal('Allie Bellew');
+      expect(profile.alias).to.equal('AllieB');
+      expect(profile.mailboxGuid).to.equal('8d899a1e-bde4-4946-8817-005e6f11d36d');
     });
   });
   
